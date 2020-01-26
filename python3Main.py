6c2085365fe3e7d70a3c8537990638725e5e0380
Braedon McDonald <braedonmcdonald1@gmail.com>
2020-01-26 01:14:09 -0500

    make changes to python3Main (it is now broken lol)

--- python3Main.py
+++ python3Main.py
@@ -1,5 +1,8 @@
 import os
 import slack
+import time
+
+
 
 @slack.RTMClient.run_on(event='message')
 def say_hello(**payload):
@@ -23,11 +26,46 @@ def say_hello(**payload):
 
         web_client.chat_postMessage(
             channel=channel_id,
-            text=f"Startin' that shit for ya <@{user}>!",
+            text=f"Starting work session!",
+            thread_ts=thread_ts
+        )
+        directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "temp_capture")
+        image_capture.start_capturing(20, 5, directory)
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"work session done. Enter \"Break\" to start your break",
+            thread_ts=thread_ts
+        )
+
+    elif 'Break' in data.get('text', []):
+        channel_id = data['channel']
+        thread_ts = data['ts']
+        user = data['user']
+
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Starting break!",
+            thread_ts=thread_ts
+        )
+        time.sleep(5 * 60)
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Break done!",
             thread_ts=thread_ts
         )
 
 #slack_token = os.environ["SLACK_BOT_TOKEN"]
-slack_token = "xoxb-924236022790-922171684224-boLaUwt35ZtvN8plCj6e12kj"
+directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "temp_capture")
+if not os.path.isdir(directory):
+    os.mkdir(directory)
+
+slack_token = "xoxb-924236022790-922171684224-S1VbuJXNAb8mlvkph90LxJqE"
 rtm_client = slack.RTMClient(token=slack_token)
 rtm_client.start()
+
+while True:
+    command, channel = parse_bot_commands(slack_client.rtm_read())
+    if command:
+        if command.startswith("calibrate"):
+
+    time.sleep(RTM_READ_DELAY)
