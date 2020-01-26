97782c6f0f7b9a1557401b78f15ffabed8ff83a9
Braedon McDonald <braedonmcdonald1@gmail.com>
2020-01-26 02:16:40 -0500

    fix slackbot commands

--- python3Main.py
+++ python3Main.py
@@ -11,29 +11,27 @@ def say_hello(**payload):
     web_client = payload['web_client']
     rtm_client = payload['rtm_client']
 
-    
-
     if 'Hello' in data.get('text', []):
         channel_id = data['channel']
         thread_ts = data['ts']
-        user = data['user']
+        #user = data['user']
 
         web_client.chat_postMessage(
             channel=channel_id,
             text=f"Hi <@{user}>!",
             thread_ts=thread_ts
         )
-    elif 'Pomodoro Start' in data.get('text', []):
+    elif 'pomodoro start' in data.get('text', []):
         channel_id = data['channel']
         thread_ts = data['ts']
-        user = data['user']
+        #user = data['user']
 
         web_client.chat_postMessage(
             channel=channel_id,
             text=f"Starting work session!",
             thread_ts=thread_ts
         )
-        directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "temp_capture")
+        directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "pomodoro")
         image_capture.start_capturing(20, 5, directory)
         web_client.chat_postMessage(
             channel=channel_id,
@@ -41,10 +39,10 @@ def say_hello(**payload):
             thread_ts=thread_ts
         )
 
-    elif 'Break' in data.get('text', []):
+    elif 'break' in data.get('text', []):
         channel_id = data['channel']
         thread_ts = data['ts']
-        user = data['user']
+        #user = data['user']
 
         web_client.chat_postMessage(
             channel=channel_id,
@@ -54,31 +52,22 @@ def say_hello(**payload):
         time.sleep(5 * 60)
         web_client.chat_postMessage(
             channel=channel_id,
-            text=f"Break done!",
+            text=f"Break done! enter \"Start\" to start another pomodoro",
             thread_ts=thread_ts
         )
 
-    elif 'calibrate right' in data.get('text', []):
+    elif 'calibrate' in data.get('text', []):
         channel_id = data['channel']
         thread_ts = data['ts']
-        user = data['user']
-        image_capture.capture_image(image_capture.calibration_directory, "calibrate-right.png")
+        #user = data['user']
+
+        direction = data.get('text', []).split(' ')[1]
+
+        image_capture.capture_image(image_capture.calibration_directory, "calibrate-" + direction + ".png")
 
         web_client.chat_postMessage(
             channel=channel_id,
-            text=f"Calibrated right side of monitor",
-            thread_ts=thread_ts
-        )
-
-    elif 'calibrate left' in data.get('text', []):
-        channel_id = data['channel']
-        thread_ts = data['ts']
-        user = data['user']
-        image_capture.capture_image(image_capture.calibration_directory, "calibrate-left.png")
-
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=f"Calibrated left side of monitor",
+            text=f"Calibrated "+ direction + " side of monitor",
             thread_ts=thread_ts
         )
 
@@ -93,8 +82,9 @@ if not os.path.isdir(image_capture.pomodoro_directory):
 if not os.path.isdir(image_capture.calibration_directory):
     os.mkdir(image_capture.calibration_directory)
 
-slack_token = "xoxb-924236022790-922171684224-BAM1JLfF35k3CLWhReFUKkwN"
+slack_token = "xoxb-924236022790-922171684224-ZGnjZPLKMbTlqPFqxGKY5uBS"
 rtm_client = slack.RTMClient(token=slack_token)
 rtm_client.start()
+
 
 
